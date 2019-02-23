6efd67d45af5c55c330b6e5680be8b0f10dc95af
ramutalari <ramu.t9@gmail.com>
2019-02-23 09:08:23 +0530

    changes

--- Learn_Automation_V1/src/package_practice_v1/JavaProgram1.java
+++ Learn_Automation_V1/src/package_practice_v1/JavaProgram1.java
@@ -5,6 +5,9 @@ public class JavaProgram1 {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
+		
+		//checking purpose
+		
 		//printing text
 		System.out.println("sample program");
 		System.out.println("commiting");
